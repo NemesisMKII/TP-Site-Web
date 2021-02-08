d0f257af2cea22e49c7336d7099e8d1ca0502a9d
Bernard Leborgne <leborgne.bernard62@gmail.com>
2021-02-08 16:37:41 +0100

    navbar

---
+++ style/default.js
@@ -0,0 +1,8 @@
+$(document).ready (() => {
+    $('#techniques').click(() => {
+        $('#technique_list').toggleClass('show')
+    })
+    $('#categories').click(() => {
+        $('#categories_list').toggleClass('show')
+    })
+})
