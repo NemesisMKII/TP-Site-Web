2f9140671d5257d08058561b3dcb8dd56cc23165
Bernard Leborgne <leborgne.bernard62@gmail.com>
2021-02-10 13:27:31 +0100

    navbar

--- style/default.js
+++ style/default.js
@@ -1,10 +1,16 @@
 $(document).ready (() => {
-    //Click on "techniques" shows techniques list
-    $('#techniques').hover((e) => {
-        $('#technique_list').toggleClass('show')
+    if ($(window).width() > 992) {
+        //Click on "techniques" shows techniques list
+        $('#techniques').hover((e) => {
+            if ($(window).width() > 992) {
+                $('#technique_list').toggleClass('show')
+            }
+        })
+        //Click on "categories" shows categories list
+        $('#categories').hover((e) => {
+            if ($(window).width() > 992) {
+                $('#categories_list').toggleClass('show')
+            }
     })
-    //Click on "categories" shows categories list
-    $('#categories').hover((e) => {
-        $('#categories_list').toggleClass('show')
-    })
+    } 
 })
