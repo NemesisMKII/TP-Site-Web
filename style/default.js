376ee8843ef4a3abd8a98752670ba1b17dfac6df
Bernard Leborgne <leborgne.bernard62@gmail.com>
2021-02-11 09:15:19 +0100

    d

--- style/default.js
+++ style/default.js
@@ -24,7 +24,9 @@ $(document).ready (() => {
     } else {
         formObj = JSON.parse(localStorage.getItem('formulaire'))
     }
-$("#myForm").submit(register)
+    
+    $("#myForm").submit(register)
+
         //envoi du formulaire
     function register(event) {
         event.preventDefault()
@@ -101,16 +103,85 @@ $("#myForm").submit(register)
         $("#mail").val("")       
         }
     }
+
+    $("#btnConnexion").click(function(e) {
+        e.preventDefault()
+        $("#modalLogin").modal("show")				//affiche le formulaire de connexion sous forme de modal
+        $("#formLogin").submit(function(event) {
+            event.preventDefault()
+            var pseudo = $("#myID").val()			//recup le pseudo
+            var MdP = $("#myPassword").val()		//recup le mot de passe
+            login(pseudo, MdP)						//lance la fonction Login
+        })
+    })
+  
+    $("#btnClose").click(function () {		//fermeture du formulaire de connection au clic sur X en haut a droite
+        $("#modalLogin").hide()
+    })
+
+    // Fonction LOGIN
+
+    function login(pseudo, MdP) {
+        var monJsonUsers								//recup du Json dans localStorage
+        if (!localStorage.getItem("localUsers")) {		//si vide, creation d'un nouveau Json
+            monJsonUsers = {"users": [{"pseudo" : "Zlatan",
+                    "mdp" : "123",
+                    "role" : "larbin"}]}
+        } else {
+            monJsonUsers = JSON.parse(localStorage.getItem("localUsers"))
+        }
+
+        if (pseudo == "" || MdP == "") {			//verif que les input pseudo et mot de passe sont rempli
+            alert("Merci de remplir les 2 champs")
+        } else {
+            var pseudoExist = false
+            let x									//recherche du pseudo dans le Json
+            for (x in monJsonUsers.users) {
+                if (pseudo == monJsonUsers.users[x].pseudo) {	//pseudo OK
+                    var monUser = monJsonUsers.users[x]
+                    pseudoExist = true
+                    break
+                } else {										//pseudo non trouvé
+                    alert("Mauvaise identification, essayez encore")
+                    location.reload()
+                }
+            }
+        }
+        if (pseudoExist) {
+            var loginOK = false
+            if (MdP == monUser.mdp) {							//MdP correspond au pseudo
+                alert("Content de vous revoir " + monUser.pseudo)
+                loginOK = true
+                sessionStorage.setItem("sessionUser", JSON.stringify(monUser))	//Stockage de l'user dans le sessionStorage
+                $("#modalLogin").hide()
+                //charger le theme s'il existe
+                //modifier le bouton "connection" en déconnection et créer la fonction
+            } else {
+                alert("Mauvaise identification, essayez encore")
+                location.reload()
+            }
+            if (loginOK) {						//pseudo + MdP OK => verif si Admin ou User
+                if (monUser.role == "admin") {
+                    // Mode Admin
+                    alert("Mode Admin enclenché")
+                } else {
+                    // User Normal
+                    alert("Mode Boudoir déverrouillé")
+                }
+            }
+        }
+    }
+
+
+// FIN Fonction LOGIN
 })
 
 
 //----------------------------------------------------
 //FOOTER
-slide = new slider("#carroussel");
 //si j'en crée une deuxieme je remet -- slide = new slider("idDuNouveauCarroussel");
 
 //-----------------------------------------------
-})
 //variable pour le footer
 var slider = function(id){
     var self=this
@@ -153,74 +224,6 @@ var slider = function(id){
            },1000)
        }
    })
-  
-  // Fonction LOGIN
-
-  $("#btnConnexion").click(function() {
-
-      $("#modalLogin").modal("show")				//affiche le formulaire de connexion sous forme de modal
-      $("#formLogin").submit(function(event) {
-          event.preventDefault()
-          var pseudo = $("#myID").val()			//recup le pseudo
-          var MdP = $("#myPassword").val()		//recup le mot de passe
-          login (pseudo, MdP)						//lance la fonction Login
-      })
-  })
-
-  $("#btnClose").click(function () {		//fermeture du formulaire de connection au clic sur X en haut a droite
-      $("#modalLogin").hide()
-  })
-
-  function login(pseudo, MdP) {
-      var monJsonUsers								//recup du Json dans localStorage
-      if (!localStorage.getItem("localUsers")) {		//si vide, creation d'un nouveau Json
-          monJsonUsers = {"users": [{"pseudo" : "Zlatan",
-                  "mdp" : "123",
-                  "role" : "larbin"}]}
-      } else {
-          monJsonUsers = JSON.parse(localStorage.getItem("localUsers"))
-      }
-
-      if (pseudo == "" || MdP == "") {			//verif que les input pseudo et mot de passe sont rempli
-          alert("Merci de remplir les 2 champs")
-      } else {
-          var pseudoExist = false
-          let x									//recherche du pseudo dans le Json
-          for (x in monJsonUsers.users) {
-              if (pseudo == monJsonUsers.users[x].pseudo) {	//pseudo OK
-                  var monUser = monJsonUsers.users[x]
-                  pseudoExist = true
-                  break
-              } else {										//pseudo non trouvé
-                  alert("Mauvaise identification, essayez encore")
-                  location.reload()
-              }
-          }
-      }
-      if (pseudoExist) {
-          var loginOK = false
-          if (MdP == monUser.mdp) {							//MdP correspond au pseudo
-              alert("Content de vous revoir " + monUser.pseudo)
-              loginOK = true
-              sessionStorage.setItem("sessionUser", JSON.stringify(monUser))	//Stockage de l'user dans le sessionStorage
-              $("#modalLogin").hide()
-              //charger le theme s'il existe
-              //modifier le bouton "connection" en déconnection et créer la fonction
-          } else {
-              alert("Mauvaise identification, essayez encore")
-              location.reload()
-          }
-          if (loginOK) {						//pseudo + MdP OK => verif si Admin ou User
-              if (monUser.role == "admin") {
-                  // Mode Admin
-                  alert("Mode Admin enclenché")
-              } else {
-                  // User Normal
-                  alert("Mode Boudoir déverrouillé")
-              }
-          }
-      }
-  }
-
-  // FIN Fonction LOGIN
 }
+
+slide = new slider("#carroussel");
