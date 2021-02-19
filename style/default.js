36b509d5a5cd413d14a609ea0af44d4bdc90b028
bouillet lise <lise_b59@yahoo.com>
2021-02-19 09:47:51 +0100

    photo categorie et techniques a modifié

--- style/default.js
+++ style/default.js
@@ -427,7 +427,7 @@ function carrousselMoove(){ //cache le carroussel et affiche sur la droite en fc
      var nameFirst = $('.choixImg').attr('name')
      var fileExtension=$('.choixImg').attr('data-ex')
 
-  $('.img-full').attr("src",way+dossier+nameFirst+'Max'+fileExtension) //zoom de la 1er image
+  $('.img-full').attr("src",way+dossier+nameFirst+'max'+fileExtension) //zoom de la 1er image
   $('.choixImg').click(function(){ //zoom au click
      //var srcImg= $(this).attr('src')
      var way = "ress/imagesCarrou/miniature/" //le chemin
@@ -441,39 +441,48 @@ function carrousselMoove(){ //cache le carroussel et affiche sur la droite en fc
 //fullScreen
 function fullScreen(){
   $('.img-full').click(function () {
-      $('#overlay').show()
+      $('.overlay').show()
       $('.imgReal').attr("src", $(this).attr('src'))
       $('#zoom').hide()
   })
   $('.closeMe').click(function () {
-      $('#overlay').hide()
+      $('.overlay').hide()
       $('#zoom').show()
   })
   $(document).keydown(function (event) {
      if (event.keyCode == 27){
-      $('#overlay').hide()
+      $('.overlay').hide()
       $('#zoom').show()
       }
   })
 }
 //End footer fct
 //--------------------------------------------------------------------------------------------
-// template pour les categories & techniques
+// template pour les categories & techniques 
+// VERIFIER AU NIVEAU DU OVERLAY (affichage en grand de la photo)
 var categoriesTemplate = 
-`<div id="centerdiv" class="h-100">
-    <div class="h-100">	
-        <div class="row h-100 align-items-center w-100">
-            <img src="%imgcategorie%" class="col-5 ms-auto border border-dark p-0 img-photographer" alt="">
-            <div class="col-7">
-                <div class="row mb-3 w-50 mx-auto">
-                    <h3 class="text-center align-self-start">%nomcategorie%</h3>
-                </div>
-            <div class="row w-75 mx-auto">
-                %textecategorie% 
+`
+<div class="overlay">
+		<img src="" class="imgReal" alt="">
+		<div class="closeMe">X</div>
+	</div>
+    <div id="centerdiv" class="h-100">
+<div class="h-100">	
+    <div class="row h-100 align-items-center w-100" id="zoom">
+        <img src="%image%"  class="col-5 ms-auto border border-dark img-full p-0 " alt="">
+        <div class="col-7">
+            <div class="row mb-3 w-50 mx-auto">
+                <h3 class="text-center align-self-start">%nomcategorie%</h3>
             </div>
+        <div class="row w-75 mx-auto">
+            %textecategorie% 
         </div>
     </div>
-</div>`
+</div>
+</div>
+`
+
+
 //fonction au click SUR categorie
     $("#catego").click(function (e){
         e.preventDefault()
@@ -482,108 +491,118 @@ var categoriesTemplate =
         $("#listeTechniques").hide() 
         $("#carroussel").hide()
     })
-   
+// fonction pour vider le main et cacher listecategories (idem pour techniques) 
+   function CategorieDiv(){
+    $("main").empty()
+    $("main").show() 
+    $("#listeCategories").hide() 
+    $("#carroussel").show()
+    fullScreen()
+    
+   }
 // Fonction click noir et blanc
-      
         $(".blackandwhite").click(function(e){
-            e.preventDefault()  
-            $("#listeCategories").hide() 
-            $("#carroussel").show()
+            e.preventDefault() 
+            CategorieDiv() 
+            var image = "ress/imagesCarrou/miniature/LPNBLion.png"
             var texte = categoriesTemplate
             var nom = "Noir & Blanc"
             var textecat = "La photographie en noir et blanc élimine les distractions La couleur peut être un très bon élément de composition. ... En supprimant ainsi les couleurs, vous réduisez grandement les sources de distractions visibles. Vous pourrez alors vous concentrer sur la forme et la texture des différents éléments de votre image."
             texte = texte.replace(/%nomcategorie%/g, nom)
             texte = texte.replace(/%textecategorie%/g , textecat)
-            $("main").empty()
+            texte = texte.replace(/%image%/g , image)
             $("main").append(texte)
-            $("main").show() 
-
+            $("footer img:not([data-cat=NB])").parent().hide();
         })
     
 // fonction au click de portrait
     $(".portrait").click(function(e){
-        e.preventDefault()   
-        $("#listeCategories").hide()
-        $("#carroussel").show()
+        e.preventDefault() 
+        CategorieDiv()   
         var texte = categoriesTemplate
         var nom = "Portrait"
+        var image = "POphoto.jpg"
         var textecat = "La photo de portrait est un art à part entière : il ne s’agit plus seulement d'immortaliser une scène ou une nature morte mais plutôt de capter une sensibilité, une personnalité, un trait d’humeur ou de caractère chez la personne qui joue les modèles devant l'objectif le temps d’une séance photo. De ce shooting photo, en studio, à domicile ou en extérieur vont naitre des images qui témoigneront d’une époque, d’une période de votre vie, d’un moment fort, peut être d’une réussite que vous aimeriez cristalliser.  Cette photo-portrait sera un concentré d'émotions pour revivre ces instants avec à chaque fois la même intensité. La photographie peut aussi avoir des vertus thérapeutiques et être un bon moyen de prendre confiance en soi, en son image, de s’accepter tel qu’on est. C’est encore le moyen de se mettre en valeur pour une photo corporate ou dans un cadre privé pour séduire de nouveaux partenaires."
         texte = texte.replace(/%nomcategorie%/g, nom)
         texte = texte.replace(/%textecategorie%/g , textecat)
-        $("main").empty()
+        texte = texte.replace(/%image%/g , image)
         $("main").append(texte) 
-        $("main").show()  
+        $("footer img:not([data-cat=por])").parent().hide(); 
     })
 //fonction au click de paysage
     $(".paysage").click(function(e){
         e.preventDefault()   
-        $("#listeCategories").hide() 
-        $("#carroussel").show()
+        CategorieDiv() 
         var texte = categoriesTemplate
         var nom = "Paysage"
+        var image = "PAphoto.jpg"
         var textecat = "Les paysages n’ont pas seulement enthousiasmés les grands peintres. D’innombrables photographes mondialement connus ont traité ce sujet fascinant. Dès l’Antiquité, des genres picturaux apparaissent. Ainsi, il existait en Grèce de grands portraitistes comme Zeuxis ou Appelle. Mais le paysage n’était pas à cette époque un genre pictural autonome. Il pouvait servir d’arrière-plan ou d’élément décoratif mais ne constituait pas en lui-même un sujet. Il faut attendre le 19e siècle pour que le paysage s’affirme comme un genre pictural dominant. Au paysage réaliste des peintres anglais ou des français de l’École de Barbizon succèdera le paysage impressionniste."
         texte = texte.replace(/%nomcategorie%/g, nom)
         texte = texte.replace(/%textecategorie%/g , textecat)
-        $("main").empty()
+        texte = texte.replace(/%image%/g , image)
         $("main").append(texte)  
-        $("main").show() 
+        $("footer img:not([data-cat=PA])").parent().hide();
     })
 // fonction au click d architecture
     $(".architecture").click(function(e){
         e.preventDefault()  
-        $("#listeCategories").hide()  
-        $("#carroussel").show()
+        CategorieDiv() 
         var texte = categoriesTemplate
         var nom = "Architecture"
+        var image = "ARphoto.jpg"
         var textecat = "La photographie d'architecture, est l’un des principaux thèmes de la photographie. Elle consiste à réaliser des photographies d’édifices, cette restitution pouvant être fidèle, ou magnifiée selon le désir, la volonté et la sensibilité artistique du photographe"
         texte = texte.replace(/%nomcategorie%/g, nom)
         texte = texte.replace(/%textecategorie%/g , textecat)
-        $("main").empty()
+        texte = texte.replace(/%image%/g , image)
         $("main").append(texte) 
-        $("main").show() 
+        $("footer img:not([data-cat=AR])").parent().hide();
     })
 // fonction au click de graphisme
 $(".graphisme").click(function(e){
     e.preventDefault() 
-    $("#listeCategories").hide()  
-    $("#carroussel").show() 
+    CategorieDiv() 
     var texte = categoriesTemplate
     var nom = "Graphisme"
+    var image = "GRphoto.jpg"
     var textecat = "Le graphisme est une discipline qui consiste à créer, choisir et utiliser des éléments graphiques (dessins, caractères typographiques, photos, couleurs, etc.) ... Le designer graphique est alors un médiateur qui agit sur les conditions de réception et d'appropriation des informations et des savoirs qu'il met en forme"
     texte = texte.replace(/%nomcategorie%/g, nom)
     texte = texte.replace(/%textecategorie%/g , textecat)
-    $("main").empty()
+    texte = texte.replace(/%image%/g , image)
     $("main").append(texte)
-    $("main").show()   
+    $("footer img:not([data-cat=GR])").parent().hide(); 
 })
  // fonction au click d evenement
 $(".evenement").click(function(e){
     e.preventDefault()   
-    $("#listeCategories").hide() 
-    $("#carroussel").show()
+    CategorieDiv() 
     var texte = categoriesTemplate
     var nom = "Evenement"
+    var image = "EVphoto.jpg"
     var textecat = "L’art de la photographie ne s’improvise pas. Entre lumières, positions, angles, cadre, vous mettre en valeur, vous et vos événements. Et au-delà de cette mise en valeur, il est votre référent pour votre image et vos souvenirs."
     texte = texte.replace(/%nomcategorie%/g, nom)
     texte = texte.replace(/%textecategorie%/g , textecat)
-    $("main").empty()
+    texte = texte.replace(/%image%/g , image)
     $("main").append(texte) 
-    $("main").show()  
+    $("footer img:not([data-cat=EV])").parent().hide();
 })   
- // fonction au click de boudoir
+ /*// fonction au click de boudoir
+ $(".boudoir").hover(function (e){
+     // a rajouter :  si l utilisateur n'est pas connecté alors 
+    alert("vous devez être connecté  ou remplir le formulaire pour accèder a cette catégorie ")
+ })*/
  // faire une fonction pour autoriser l'acces a cette categorie
  $(".boudoir").click(function(e){
+    alert("vous devez être connecté ou remplir le formulaire pour accéder a ce formulaire" )
     e.preventDefault()  
-    $("#listeCategories").hide() 
-    $("#carroussel").show() 
+    CategorieDiv() 
     var texte = categoriesTemplate
     var nom = "Boudoir"
+    var image = "BOphoto.jpg"
     var textecat = "La photo boudoir mêle féminité, douceur, sensualité et émotion. A la différence de la photo de nu, les photos boudoir sont généralement prises dans un décor naturel, en studio, en chambre ou dans votre salon afin de vous représenter dans votre univers."
     texte = texte.replace(/%nomcategorie%/g, nom)
     texte = texte.replace(/%textecategorie%/g , textecat)
-    $("main").empty()
+    texte = texte.replace(/%image%/g , image)
     $("main").append(texte) 
-    $("main").show()  
 })   
 
 //------------------------------------------------------------------------------------------------
@@ -596,105 +615,113 @@ $("#tech").click(function (e){
     $("#listeCategories").hide() 
     $("#carroussel").hide()
 })
+// vider et cacher listetechniques
+function techniquesDiv(){
+    $("main").empty()
+    $("main").show() 
+    $("#listeTechniques").hide() 
+    $("#carroussel").show()
+   }
 
  // fonction au click de HDR
  $(".hdr").click(function(e){
     e.preventDefault() 
-    $("#listeTechniques").hide()  
-    $("#carroussel").show()
+    techniquesDiv()
     var texte = categoriesTemplate
     var nom = "HDR"
+    var image = "HDphoto.png"
     var textecat = "Le HDR (abréviation du terme High Dynamic Range signifiant grande plage dynamique) désigne des formats et des standards de vidéos et d'images numériques permettant de représenter des niveaux de luminosités plus élevés que la limite de 100 nits des contenus du standard SDR. Le HDR permet d'augmenter la gamme dynamique pouvant être enregistrée dans une image ou une vidéo numérique et permet d'obtenir des hautes-lumières plus lumineuses, plus détaillées et plus saturées en couleur. Les technologies liées au HDR permettent également d'améliorer le détail dans les zones sombres."
     texte = texte.replace(/%nomcategorie%/g, nom)
     texte = texte.replace(/%textecategorie%/g , textecat)
-    $("main").empty()
-    $("main").append(texte) 
-    $("main").show()  
+    texte = texte.replace(/%image%/g , image)
+    $("main").append(texte)   
+    $("footer img:not([data-tec=HD])").parent().hide();
 }) 
 // click high key low key
 $(".highkey").click(function(e){
     e.preventDefault()  
-    $("#listeTechniques").hide()
-    $("#carroussel").show()  
+    techniquesDiv() 
     var texte = categoriesTemplate
     var nom = "High Key/Low Key"
+    var image = "HKphoto.jpg"
     var textecat = "Une photo High-key est une image qui présente une forte prédominance des teintes claires. Légèrement surexposé sans pour autant être «brûlé » et dépourvu d’ombres, le portrait high-key se présente comme une image très claire, presque blanche sur laquelle, seuls quelques reliefs ou détails stratégiques plus foncés sont mis en évidence. Le portrait high-key propose une vision légère, romantique, douce et pure d’un visage en lumière diffuse et uniforme.<br>Une photo Low-key, quant à elle, est une image qui présente une prédominance des teintes foncées. Volontairement sous-exposé, le portrait low-key regroupe plusieurs styles d’images dont les visuels en clair-obscur. Réalisé en général avec une seule source lumineuse, le portrait low-key se présente comme une image noire sur laquelle, seuls quelques reliefs ou détails stratégiques ont été mis en lumière. Au contraire du high-key, le portrait low-key propose une vision caractérisée d’un visage en lumière contrastée."
     texte = texte.replace(/%nomcategorie%/g, nom)
     texte = texte.replace(/%textecategorie%/g , textecat)
-    $("main").empty()
+    texte = texte.replace(/%image%/g , image)
     $("main").append(texte)  
-    $("main").show() 
+    $("footer img:not([data-tec=HK])").parent().hide();
 })  
 // click high speed
 $(".highspeed").click(function(e){
     e.preventDefault()   
-    $("#listeTechniques").hide() 
-    $("#carroussel").show()
+    techniquesDiv()
+    var image = "HSphoto.jpg"
     var texte = categoriesTemplate
     var nom = "High Speed"
     var textecat = "La photographie High Speed est une approche technique répondant à deux enjeux : une durée d'exposition aussi courte que possible et un déclenchement précis dans le temps pour le synchroniser avec le phénomène à saisir. Le but est de capter des phénomènes trop rapides pour être perçus à l'œil nu."
     texte = texte.replace(/%nomcategorie%/g, nom)
     texte = texte.replace(/%textecategorie%/g , textecat)
-    $("main").empty()
+    texte = texte.replace(/%image%/g , image)
     $("main").append(texte) 
-    $("main").show()  
+    $("footer img:not([data-tec=HS])").parent().hide();
 }) 
 // click studio
 $(".studio").click(function(e){
     e.preventDefault()  
-    $("#listeTechniques").hide() 
-    $("#carroussel").show() 
+    techniquesDiv()
     var texte = categoriesTemplate
     var nom = "Studio"
+    var image = "STphoto.jpg"
     var textecat = "Profitez de la qualité studio professionnelle, composé de plusieurs flashs avec diffuseur de lumière et de fonds unis ou imprimés et une multitude d'accessoires et vêtements."
     texte = texte.replace(/%nomcategorie%/g, nom)
     texte = texte.replace(/%textecategorie%/g , textecat)
-    $("main").empty()
+    texte = texte.replace(/%image%/g , image)
     $("main").append(texte)
-    $("main").show()   
+    $("footer img:not([data-tec=ST])").parent().hide();
 }) 
 // click tilt shift
 $(".tiltshift").click(function(e){
     e.preventDefault() 
-    $("#listeTechniques").hide() 
-    $("#carroussel").show()  
+    techniquesDiv()
     var texte = categoriesTemplate
     var nom = "Tilt Shift"
+    var image = "TSphoto.jpg"
     var textecat = "Le Tilt-Shift, plus communément appelé effet maquette, est à la base un effet créé par les objectifs à bascule. Il permet -grâce à un flou qui entoure le sujet- de donner une impression de miniaturisation à une photo."
     texte = texte.replace(/%nomcategorie%/g, nom)
     texte = texte.replace(/%textecategorie%/g , textecat)
-    $("main").empty()
+    texte = texte.replace(/%image%/g , image)
     $("main").append(texte)  
-    $("main").show() 
+    $("footer img:not([data-tec=TS])").parent().hide();
 }) 
 // click POSE LONGUE
 $(".poselongue").click(function(e){
     e.preventDefault()   
-    $("#listeTechniques").hide() 
-    $("#carroussel").show()
+    techniquesDiv()
     var texte = categoriesTemplate
     var nom = "Pose Longue"
+    var image = "PLphoto.jpg"
     var textecat = "Une pose longue consiste, en photographie, à utiliser, de jour, un temps de pose assez long pour capturer nettement les éléments fixes d'une scène tout en supprimant les éléments en mouvement ou en les rendant flous et, de nuit, à transformer les sources lumineuses mobiles en trainées lumineuses"
     texte = texte.replace(/%nomcategorie%/g, nom)
     texte = texte.replace(/%textecategorie%/g , textecat)
-    $("main").empty()
+    texte = texte.replace(/%image%/g , image)
     $("main").append(texte)
-    $("main").show()  
+    $("footer img:not([data-tec=PL])").parent().hide(); 
 }) 
 // click light painting
 $(".lightpainting").click(function(e){
     e.preventDefault() 
-    $("#carroussel").show()
-    $("#listeTechniques").hide()   
+    techniquesDiv() 
     var texte = categoriesTemplate
     var nom = "Light Painting"
+    var image = "LPphoto.jpg"
     var textecat = "Le light painting est une technique visuelle de prise de vue photographique fondée sur la captation de la lumière, peu importe sa forme et son intensité, sur un capteur optique ou numérique. Cela permet de fixer la lumière dans un état temporel et d’espace."
     texte = texte.replace(/%nomcategorie%/g, nom)
     texte = texte.replace(/%textecategorie%/g , textecat)
-    $("main").empty()
+    texte = texte.replace(/%image%/g , image)
     $("main").append(texte) 
-    $("main").show()  
+    $("footer img:not([data-tec=LP])").parent().hide(); 
 }) 
+
  //----------------------------------------------------------------------------------------------       
 // a l'ext de onready
 function captcha_new(){
